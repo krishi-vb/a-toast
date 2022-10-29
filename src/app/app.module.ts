198d99405dd5b41ce9c7d807a3221b815a28dafa
Krishi V Bhat <vb.krishi@gmail.com>
2022-10-29 13:51:06 +0530

    new toast component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtnContainerComponent } from './btn-container/btn-container.component';
+import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
-  declarations: [AppComponent, BtnContainerComponent],
+  declarations: [AppComponent, BtnContainerComponent, ToastComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
