eac6134f6c928ffe5e589af6f6024e735f65891c
Krishi V Bhat <vb.krishi@gmail.com>
2022-10-29 13:35:22 +0530

    initial commit with just buttons

---
+++ src/app/btn-container/btn-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-container',
+  templateUrl: './btn-container.component.html',
+  styleUrls: ['./btn-container.component.scss']
+})
+export class BtnContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
