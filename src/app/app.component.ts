a7b867965abf98e3a2e1bf961bc50385b5aa013d
Krishi V Bhat <vb.krishi@gmail.com>
2022-10-30 21:11:15 +0530

    cleaned up app component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,9 @@
 import { Component } from '@angular/core';
 import { ToastEvent } from './toast/toast.types';
 
-export interface LogData
-  extends Omit<
-    ToastEvent,
-    'body' | 'header' | 'headerColor' | 'bodyColor' | 'buttonText'
-  > {
-  time?: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  toastEnabled: boolean = false;
-  clickLogs: LogData[] = [];
-  notifications: ToastEvent[] = [];
-  buttonThatWasClicked: string = '';
-  toastData!: ToastEvent;
-
-  onBtnClick(data: ToastEvent) {
-    this.notifications.push(data);
-    let log: LogData = { type: data.type };
-    log.time = new Date().toLocaleTimeString();
-    this.clickLogs.push(log);
-    this.toastData = data;
-    this.toastEnabled = true;
-    console.log('click was registered');
-    console.log(this.notifications);
-  }
-}
+export class AppComponent {}
